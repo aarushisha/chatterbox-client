e2b2e9deb925f8096433a365da963dacc4825ea1
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-01-09 15:51:55 -0800

    Complete Bare Minimum Requirements

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -3,14 +3,30 @@ var Friends = {
   allFriends: [],
 
   add: function(friend) {
-    allFriends.push(friend)
+    Friends.allFriends.push(friend)
   },
 
 
   toggleStatus: function() {
   // console.log('clicked username');
   // console.log($('.username'));
-  console.log($('.username').text());
+  var allUsers = document.getElementsByClassName("username");
+  var allMessages = document.getElementsByClassName("chat");
+  console.log(allMessages);
+  var newFriend = this.textContent;
+  console.log(newFriend);
+  for (var i = 0; i < allUsers.length; i++) {
+    // console.log(this.textContent);
+    console.log(App.username);
+    if (allUsers[i].textContent !== App.username && allUsers[i].textContent === this.textContent) {
+      Friends.allFriends.push(allUsers[i].textContent);
+      console.log(Friends.allFriends);
+      allUsers[i].style.color = '#84D9FF';
+      allMessages[i].style.fontWeight = 'bold';
+      allMessages[i].style.color = '#670CE8';
+    }
+  }
+
   }
 
 
